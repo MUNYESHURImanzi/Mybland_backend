2e88f1ddf1879d23a0a98919cd4a50e28dded1a7
ManziPatrick <patrick.manzi@amalitech.org>
2024-04-05 09:37:30 +0200

    blog

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -3,7 +3,14 @@ import { signupUser, loginUser } from '../contrallers/user';
 import Contact from '../contrallers/contact';
 
 export default (router: express.Router) => {
+ 
     router.post('/signup', signupUser);
+
     router.post('/login', loginUser);
-    router.post('/contact',Contact)
+
+    router.post('/contact', Contact);
+
+    router.get('/', (req, res) => {
+        res.send('Welcome to the application!');
+    });
 };
