ce59c9894d8c1c8975b4ca5daaa7a543cd50e7a6
ManziPatrick <patrick.manzi@amalitech.org>
2024-04-16 22:31:49 +0200

    blog

--- dist/middleware/UserMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authenticateUser = void 0;
-const authenticateUser = (req, res, next) => {
-    next();
-};
-exports.authenticateUser = authenticateUser;
